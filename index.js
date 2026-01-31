dd9d86f51624e490f8286a66d8c5ce8cd3ad176a
gn-bio <willblackwood39@gmail.com>
2026-01-31 11:11:56 -0800

    update index.html, index.js, and style.css

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ function sendMail() {
         whatNeedHelpWith : document.getElementById("whatNeedHelpWith").value,
         moreInfo : document.getElementById("moreInfo").value
     }
+    if ((document.getElementById("whatNeedHelpWith").value === "") || (document.getElementById("moreInfo").value === "")) {
+        window.alert("Please fill out questions 1 and 2 before submitting.");
+        return -1;
+    }
     try {
         console.log('before send');
         emailjs.send("service_eucl8wo","template_gjy0yka",params).then(window.alert("Support message sent!"))
